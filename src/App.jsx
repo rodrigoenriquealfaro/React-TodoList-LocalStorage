3a6ac1289a3c2b955d2b1c1e836e141435cab27c
Rodrigo Enrique Alfaro <rodrigoenriquealfaro@hotmail.com>
2022-06-17 06:47:57 -0300

    Primer Commit TodoList Version 1

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react"
+import Container from "./components/Container"
+import TaskCreator from "./components/TaskCreator"
+import TaskTable from "./components/TaskTable"
+import VisibilityControl from "./components/VisibilityControl"
+
+const App = () => {
+    const [taskItems, setTaskItems] = useState([])
+    const [showCompleted, setShowCompleted] = useState(false)
+
+    const createNewTask = (taskName) => {
+        console.log(taskName)
+        if (!taskItems.find(task => task.name === taskName)) {
+            setTaskItems([...taskItems, { name: taskName, done: false }])
+        }
+    }
+
+    const toggleTask = (task) => {
+        setTaskItems(
+            taskItems.map((t) => (t.name === task.name) ? { ...t, done: !t.done } : t)
+        )
+    }
+
+    useEffect(() => {
+        let data = localStorage.getItem('tasks')
+        if (data) {
+            setTaskItems(JSON.parse(data))
+        }
+    }, [])
+
+    const cleanTasks = () => {
+        setTaskItems(taskItems.filter(task => !task.done))
+        setShowCompleted(false)
+    }
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(taskItems))
+    }, [taskItems])
+
+    return (
+        <main className="bg-dark vh-100 text-white">
+            <Container>
+                <TaskCreator createNewTask={createNewTask} />
+                <TaskTable tasks={taskItems} toggleTask={toggleTask} />
+                <VisibilityControl
+                    isChecked={showCompleted}
+                    setShowCompleted={(checked) => setShowCompleted(checked)}
+                    cleanTasks={cleanTasks}
+                />
+
+                {showCompleted && (
+                    <TaskTable tasks={taskItems} toggleTask={toggleTask} showCompleted={showCompleted} />
+                )}
+            </Container>
+        </main>
+    )
+}
+
+export default App
---
+++ src/components/TaskCreator.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+const TaskCreator = ({ createNewTask }) => {
+    const [newTaskName, setNewTaskName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        createNewTask(newTaskName)
+        setNewTaskName('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className='my-2 row'>
+            <div className="col-9">
+                <input
+                    type="text"
+                    placeholder="Enter a new Task"
+                    value={newTaskName}
+                    onChange={(e) => setNewTaskName(e.target.value)}
+                    className='form-control'
+                />
+            </div>
+            <div className="col-3">
+                <button className="btn btn-primary btn-sm">Save Task</button>
+            </div>
+        </form>
+    )
+}
+
+export default TaskCreator
